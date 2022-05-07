fcd5d2aea04be8cf3c207c4970ed453c7709a2b4
akruhoberec1 <50674565+akruhoberec1@users.noreply.github.com>
2022-05-07 14:55:34 +0200

    Update Frizeraj.sql

--- Frizeraj.sql
+++ Frizeraj.sql
@@ -1,37 +1,40 @@
 drop database if exists frizeraj;
-create database frizeraj;
+create database frizeraj default character set utf8;
 use frizeraj;
 
 create table usluge(
-sifra int(1),
+sifra int not null primary key auto_increment,
 naziv varchar(60),
-cijena DECIMAL(4,2)
+cijena DECIMAL(18,2) DEFAULT 5000
 );
 
 create table rezervacija(
-sifra int(1),
-datum DATE(DEFAULT),
-vrijeme time(DEFAULT),
-usluga int(1)
+ssifra int not null primary key auto_increment,
+datumvrijeme NULL,
+"kasnije postavljamo datumvrijeme kroz funkciju s get datetime()"
+usluga int not NULL
 );
 
 create table osoba(
-idosobe int(1),
+idosobe int not null primary key auto_increment,
 imepre varchar(60),
 spol CHAR(1),
 oib int(11)
 );
 
 create table zaposlenici(
-iddjelatnik int(1),
+iddjelatnik int not null primary key auto_increment,
 osoba int(1),
-iban varchar(20),
-rezervacija int(1)
+iban varchar(20)
 );
 
 create table korisnici(
-idklijent int(1),
-osoba int(1),
-rezervacija int(1)
+idklijent int not null primary key auto_increment,
+osoba int(1)
+);
+
+create table poveznica(
+zaposlenici int not null,
+korisnici int not NULL
 );
 
